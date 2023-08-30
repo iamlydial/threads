bec06ef81a70ac6c978fdaa8e8dce1c4b1d03c68
iamlydial <lydia.lavecchia@gmail.com>
2023-08-30 15:32:40 +0200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        {/* <UserButton afterSignOutUrl="/" /> */}
+        <h1 className="head-text text-left">Home</h1>
+      </div>
+    </>
+  );
+}
