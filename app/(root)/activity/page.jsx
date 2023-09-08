dc812c93108e51121368d0c4cdf72586e96a12f6
iamlydial <lydia.lavecchia@gmail.com>
2023-09-08 20:32:49 +0200

    centered login, added activity component functionality

--- app/(root)/activity/page.jsx
+++ app/(root)/activity/page.jsx
@@ -1,6 +1,7 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import Image from "next/image";
 
 async function Page() {
   const user = await currentUser();
@@ -10,11 +11,29 @@ async function Page() {
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   //get activity
-  
+  const activity = await getActivity(userInfo._id);
 
   return (
-    <section>
-      <h1 className="head-text mb-10">Activity</h1>
+    <section className="mt-10 flex flex-col gap-5">
+      {activity.length > 0 ? (
+        <>
+          {activity.map((activity) => (
+            <Link key={activity._id} href={`/threas/${activity.parentId}`}>
+              <article className="activity-card">
+                <Image
+                  src={activity.author.image}
+                  alt="Profile Picture"
+                  width={20}
+                  height={20}
+                  className="rounded-full object-cover"
+                />
+              </article>
+            </Link>
+          ))}
+        </>
+      ) : (
+        <p className="!text-base-regular text-light-3">No activity yet</p>
+      )}
     </section>
   );
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -141,7 +141,7 @@ export async function getActivity(userId: string) {
     //collect all child threads ids (replies) from the user
     const childThreadIds = userThreads.reduce((acc, userThread) => {
       return acc.concat(userThread.children);
-    });
+    }, []);
     const replies = await Thread.find({
       _id: { $in: childThreadIds },
       author: { $ne: userId },
