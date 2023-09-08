91b4abd4d4b71889495a1c7581faa0d57927d9ff
iamlydial <lydia.lavecchia@gmail.com>
2023-09-08 08:50:08 +0200

    search action and page added for user search

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,11 @@
 "use server";
 
+import { SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import Thread from "../models/thread.model";
 import { connectToDB } from "../mongoose";
+import { FilterQuery } from "mongoose";
 // import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
 interface Params {
@@ -60,25 +62,73 @@ export async function fetchUser(userId: string) {
   }
 }
 
-export async function fetchUserPosts(userId: string){
-  try{
+export async function fetchUserPosts(userId: string) {
+  try {
     connectToDB();
-    //find all threads autored by user 
-    const threads = await User.findOne({ id: userId })
-    .populate({
-      path: 'threads', 
-      model: Thread, 
+    //find all threads autored by user
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
       populate: {
-        path: 'children', 
-        model: Thread, 
+        path: "children",
+        model: Thread,
         populate: {
-          path: 'author', 
-          model: User, 
-          select: 'name image id',
-        }
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts ${error.message}`);
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber: number;
+  pageSize: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = { id: { $ne: userId } };
+
+    if (searchString?.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
     }
-  })
-  return threads; 
-} catch(error: any){
-  throw new Error(`Failed to fetch user posts ${error.message}`)
-}}
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error: any) {
+    throw new Error(`Failed to fetchusers: ${error.message}`);
+  }
+}
